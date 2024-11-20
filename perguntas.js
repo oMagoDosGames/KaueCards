b9b89083f000061599ed05d9093ef6ed63115009
oMagoDosGames <00001096838151sp@al.educacao.sp.gov.br>
2024-11-20 14:40:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Biologia',
+'O que é uma espécie invasora?',
+'As espécies exóticas invasoras são aquelas plantas e animais que estão fora da sua área de distribuição natural e que ameaçam hábitats, serviços ecossistêmicos, e a diversidade biológica, causando impactos em ambientes naturais.'
+
+
+)
+criaCartao(
+    'Biologia',
+'O que é o RNA?',
+'O RNA é um ácido nucleico que se relaciona com a síntese de proteínas'
+
+
+)
+criaCartao(
+    'Biologia',
+'O que é o DNA?',
+' O DNA é um ácido nucleico que apresenta todas as informações genéticas de um indivíduo.'
+)
+criaCartao(
+    'Pogramação',
+'O que é o C++?',
+' C++ é uma linguagem de programação de propósito geral, criada como uma extensão da linguagem C.'
+
+
+)
+
+criaCartao(
+    'Pogramação',
+'O que é a Função?',
+'Uma função é um bloco de código que executa alguma operação.'
+
+
+)
+criaCartao(
+    'Pogramação',
+'O que é um HTML?',
+' O HTML é o bloco de construção mais básico da web, ele Define o significado e a estrutura do conteúdo da web.'
+
+)
+
